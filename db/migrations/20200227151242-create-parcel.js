788a7c659f917f023ea0e688458d69f0b30dd71f
TheMindset <mulezig2014@outlook.fr>
2020-02-27 16:36:42 +0100

    Add Parcel table

---
+++ db/migrations/20200227151242-create-parcel.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Parcels', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      weight: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      volume: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      recipient: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      zipcode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Parcels');
+  }
+};
---
+++ models/parcel.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Parcel = sequelize.define('Parcel', {
+    type: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: {
+          args: [['CLASSIC', 'EXPRESS']],
+          msg: "Type must be between CLASSIC and EXPRESS"
+        } 
+      },
+      weight: DataTypes.FLOAT,
+      volume: DataTypes.FLOAT,
+      recipient: DataTypes.STRING,
+      address: DataTypes.STRING,
+      city: DataTypes.STRING,
+      zipcode: DataTypes.STRING
+    },
+  }, {});
+  Parcel.associate = function(models) {
+    // associations can be defined here
+  };
+  return Parcel;
+};
