52d115398e648352c00f4736d07e8114e736562b
TheMindset <mulezig2014@outlook.fr>
2020-02-29 13:31:52 +0100

    Add register endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/api/v1/users');
 const parcelRouter = require('./routes/api/v1/parcels')
 
 const app = express();
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/parcels', parcelRouter)
 
 // catch 404 and forward to error handler
